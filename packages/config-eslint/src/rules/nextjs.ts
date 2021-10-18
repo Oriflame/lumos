52cd73a00f2675ede85f8aac76e354cec4a0c069
Jakub Mazanec <jakub.mazanec@oriflame.com>
2021-10-18 15:22:24 +0200

    fix: Correct Next.js ESLint plugin rule names. (#842)
    * fix: Correct Next.js ESLint plugin rule names.
    
    * tests: Fix failing tests.
    
    * Revert "tests: Fix failing tests."
    
    This reverts commit ed4539876b48aa8dbae7acae1458cc7921d01e1c.
    
    * fix: Caniuse lite.
    
    * fix: Package json.
    
    * fix: Add resolution.
    
    Co-authored-by: Jan Šilhan <silhanu@gmail.com>

--- packages/config-eslint/src/rules/nextjs.ts
+++ packages/config-eslint/src/rules/nextjs.ts
@@ -1,19 +1,19 @@
 import { ESLintConfig } from '@beemo/driver-eslint';
 
 const config: ESLintConfig['rules'] = {
-  'next/google-font-display': 'off', // enforce optional or swap font-display behavior with Google Fonts
-  'next/google-font-preconnect': 'off', // enforce preconnect usage with Google Fonts
-  'next/link-passhref': 'warn', // enforce passHref prop usage with custom Link components
-  'next/no-css-tags': 'off', // prevent manual stylesheet tags
-  'next/no-document-import-in-page': 'error', // disallow importing next/document outside of pages/document.js
-  'next/no-head-import-in-document': 'error', // disallow importing next/head in pages/document.js
-  'next/no-html-link-for-pages': 'error', // prohibit HTML anchor links to pages without a Link component
-  'next/no-img-element': 'error', // prohibit usage of HTML <img> element
-  'next/no-page-custom-font': 'off', // prevent page-only custom fonts
-  'next/no-sync-scripts': 'warn', // forbid synchronous scripts
-  'next/no-title-in-document-head': 'error', // disallow using <title> with Head from next/document
-  'next/no-unwanted-polyfillio': 'off', // prevent duplicate polyfills from Polyfill.io
-  'next/no-typos': 'warn', // ensure no typos were made declaring Next.js's data fetching function
+  '@next/next/google-font-display': 'off', // enforce optional or swap font-display behavior with Google Fonts
+  '@next/next/google-font-preconnect': 'off', // enforce preconnect usage with Google Fonts
+  '@next/next/link-passhref': 'warn', // enforce passHref prop usage with custom Link components
+  '@next/next/no-css-tags': 'off', // prevent manual stylesheet tags
+  '@next/next/no-document-import-in-page': 'error', // disallow importing next/document outside of pages/document.js
+  '@next/next/no-head-import-in-document': 'error', // disallow importing next/head in pages/document.js
+  '@next/next/no-html-link-for-pages': 'error', // prohibit HTML anchor links to pages without a Link component
+  '@next/next/no-img-element': 'error', // prohibit usage of HTML <img> element
+  '@next/next/no-page-custom-font': 'off', // prevent page-only custom fonts
+  '@next/next/no-sync-scripts': 'warn', // forbid synchronous scripts
+  '@next/next/no-title-in-document-head': 'error', // disallow using <title> with Head from next/document
+  '@next/next/no-unwanted-polyfillio': 'off', // prevent duplicate polyfills from Polyfill.io
+  '@next/next/no-typos': 'warn', // ensure no typos were made declaring Next.js's data fetching function
 };
 
 export = config;
