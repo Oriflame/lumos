87928be0da9ac8342b84e0b8ec045d54df3b54e7
Jan Šilhan <silhanu@gmail.com>
2021-10-21 13:20:12 +0200

    new: Add azure pr check. (#853)

---
+++ packages/lumos/src/helpers/createAzClient.ts
@@ -0,0 +1,25 @@
+import { Octokit } from '@octokit/rest';
+
+import { VERSION } from '../constants';
+
+export function createAzClient(token?: string): Octokit {
+  const { GITHUB_TOKEN, GHE_API_URL, GHE_VERSION } = process.env;
+  const options: Octokit.Options = {
+    userAgent: `Lumos v${VERSION}`,
+  };
+
+  if (token ?? GITHUB_TOKEN) {
+    options.auth = `token ${token ?? GITHUB_TOKEN ?? ''}`;
+  }
+
+  if (GHE_API_URL) {
+    options.baseUrl = GHE_API_URL;
+  }
+
+  if (GHE_VERSION) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument -- we need to use require()
+    Octokit.plugin(require(`@octokit/plugin-enterprise-rest/ghe-${GHE_VERSION}`));
+  }
+
+  return new Octokit(options);
+}
