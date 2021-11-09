4532f628777aa15b91a10f80cf87bff056630618
Jan Šilhan <silhanu@gmail.com>
2021-11-09 09:02:52 +0100

    Revert "deps: update dependency eslint to v8 (#827)" (#890)
    This reverts commit 6fcd4ea61532eb05185059d849e2862ff5b27cf0.

--- packages/config-eslint/test/index.test.ts
+++ packages/config-eslint/test/index.test.ts
@@ -8,8 +8,24 @@ interface OptionsShape {
 describe('Check that eslint rules are correct', () => {
   test.each<[ESLintOptions, string, OptionsShape?]>([
     [{}, 'javascript.js', undefined],
+    [
+      { typescript: true, future: true },
+      'typescript.ts',
+      { filePath: `${process.cwd()}/packages/config-eslint/src/index.ts` },
+    ],
     [{ node: true }, 'node.js', undefined],
+    [{ future: true }, 'next.js', undefined],
     [{ prettier: true }, 'prettier.js', undefined],
+    [
+      { future: true, node: false, prettier: true, typescript: true },
+      'test.js',
+      { filePath: 'index.test.js' },
+    ],
+    [
+      { future: true, node: false, prettier: true, typescript: true },
+      'combo.ts',
+      { filePath: `${process.cwd()}/packages/config-eslint/src/index.ts` },
+    ],
   ])(
     'Config (options: %o) should work with file: %s and cliOptions: %o',
     async (options, file, cliOptions) => {
@@ -27,8 +43,27 @@ describe('Check that eslint rules are correct', () => {
 
   test.each<[ESLintOptions, string, OptionsShape | undefined, number]>([
     [{}, 'javascript.js', undefined, 1],
+    [
+      { typescript: true, future: true },
+      'typescript.ts',
+      { filePath: `${process.cwd()}/packages/config-eslint/src/index.ts` },
+      1,
+    ],
     [{ node: true }, 'node.js', undefined, 1],
+    [{ future: true }, 'next.js', undefined, 2],
     [{ prettier: true }, 'prettier.js', undefined, 4],
+    [
+      { future: true, node: false, prettier: true, typescript: true },
+      'test.js',
+      { filePath: 'index.test.js' },
+      1,
+    ],
+    [
+      { future: true, node: false, prettier: true, typescript: true },
+      'combo.ts',
+      { filePath: `${process.cwd()}/packages/config-eslint/src/index.ts` },
+      1,
+    ],
   ])(
     'Config (options: %o) should throw error with file: %s and cliOptions: %o',
     async (options, file, cliOptions, expectedErrors) => {
