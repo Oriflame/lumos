29aa25faeee5cecbfb92912482a28f632214c050
Jan Šilhan <silhanu@gmail.com>
2021-12-03 09:58:24 +0100

    internal: Update confix.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   "extends": [
-    "./packages/config-eslint/lib/presets/base",
-    "./packages/config-eslint/lib/presets/node",
-    "./packages/config-eslint/lib/presets/typescript",
-    "./packages/config-eslint/lib/presets/prettier",
+    "./packages\\config-eslint\\lib\\presets\\base",
+    "./packages\\config-eslint\\lib\\presets\\node",
+    "./packages\\config-eslint\\lib\\presets\\typescript",
+    "./packages\\config-eslint\\lib\\presets\\prettier",
     "./packages/config-eslint/lib/presets/SECRET_CONFIG_DO_NOT_USE_THIS"
   ],
   "env": {
