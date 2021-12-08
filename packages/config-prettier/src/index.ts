e97715888d6838815ea1fb326b5985c8a0fa4217
Jan Šilhan <silhanu@gmail.com>
2021-12-08 13:06:29 +0100

    fix: Add new config.

--- packages/config-prettier/src/index.ts
+++ packages/config-prettier/src/index.ts
@@ -1,22 +1,10 @@
-import { PrettierConfig } from '@beemo/driver-prettier';
+import type { PrettierConfig } from '@beemo/driver-prettier';
 import { IGNORE_PATHS } from '@oriflame/lumos-common';
 
 export { PrettierConfig };
 
 const config = {
-  printWidth: 100,
-  tabWidth: 2,
-  useTabs: false,
-  semi: true,
-  singleQuote: true,
-  quoteProps: 'as-needed',
-  jsxSingleQuote: false,
-  trailingComma: 'all',
-  bracketSpacing: true,
-  bracketSameLine: false,
-  arrowParens: 'always',
-  requirePragma: false,
-  proseWrap: 'always',
+  ...(require('prettier-config-oriflame') as PrettierConfig),
   ignore: [
     ...IGNORE_PATHS,
     'npm-shrinkwrap.json',
