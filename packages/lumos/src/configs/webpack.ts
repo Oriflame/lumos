4d6a640e47b072cf4504867002e068511df115af
rajzik <jsilhan@oriflame.com>
2021-12-16 15:59:04 +0100

    internal: Experiment with settings reading.

--- packages/lumos/src/configs/webpack.ts
+++ packages/lumos/src/configs/webpack.ts
@@ -1,14 +1,28 @@
+import { BeemoConfig } from '@beemo/core';
+import { PathResolver } from '@boost/common';
+import { requireModule } from '@boost/module';
 import { getConfig } from '@oriflame/config-webpack';
 
 import { getSettings } from '../helpers/getSettings';
 import { LumosSettings } from '../types';
 
-const { tool = {} } = process.lumos || process.beemo;
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+const tool = (process.lumos || process.beemo)?.tool;
 
-// Workaround for beemo
-const envSettings = JSON.parse(process.env.LUMOS_SETTINGS || 'null') as LumosSettings;
+let lumosSettings: Partial<LumosSettings> | undefined;
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+if (!tool) {
+  const resolver = new PathResolver();
 
-const settings = getSettings(tool, envSettings);
+  resolver
+    .lookupFilePath('.config/lumos.ts', process.cwd())
+    .lookupFilePath('.config/lumos.js', process.cwd());
+
+  const path = await resolver.resolvePath();
+  lumosSettings = requireModule<BeemoConfig<Partial<LumosSettings>>>(path).default.settings;
+}
+
+const settings = getSettings(tool, lumosSettings);
 
 const {
   srcFolder,
